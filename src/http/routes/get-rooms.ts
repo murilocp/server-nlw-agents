52147eebbdce9f49e44702ba08454daf154c5f17
Murilo Picinato <mcpicinato@gmail.com>
2025-07-07 23:36:23 -0300

    add rooms

---
+++ src/http/routes/get-rooms.ts
@@ -0,0 +1,14 @@
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import { db } from '../../db/connection.ts';
+import { schema } from '../../db/schema/index.ts';
+
+export const getRoomsRoute: FastifyPluginCallbackZod = app => {
+  app.get('/rooms', async () => {
+    const results = await db
+      .select()
+      .from(schema.rooms)
+      .orderBy(schema.rooms.createdAt);
+
+    return results;
+  });
+};
