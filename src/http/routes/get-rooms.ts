4716d36ec4fc5452eb8f59fc157d8f704cb02581
Murilo Picinato <mcpicinato@gmail.com>
2025-07-08 00:29:14 -0300

    ajustar select

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -5,7 +5,10 @@ import { schema } from '../../db/schema/index.ts';
 export const getRoomsRoute: FastifyPluginCallbackZod = app => {
   app.get('/rooms', async () => {
     const results = await db
-      .select()
+      .select({
+        id: schema.rooms.id,
+        name: schema.rooms.name,
+      })
       .from(schema.rooms)
       .orderBy(schema.rooms.createdAt);
 
