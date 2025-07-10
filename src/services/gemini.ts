c59d6ce19bc487f3abb276f4cc10f8ba84fca09a
Murilo Picinato <mcpicinato@gmail.com>
2025-07-09 21:21:39 -0300

    finalizar funcionalidades

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -44,3 +44,44 @@ export async function generateEmbeddings(text: string) {
 
   return response.embeddings[0].values;
 }
+
+export async function generateAnswer(
+  question: string,
+  transcriptions: string[],
+) {
+  const context = transcriptions.join('\n\n');
+
+  const prompt = `
+  Com base no texto fornecido abaixo como contexto, responda à pergunta de forma clara e precisa em português do Brasil. 
+
+  Contexto: 
+  ${context}
+  
+  Pergunta: 
+  ${question}
+
+  Instruções:
+  - Use apenas informações contidas no contexto fornecido.
+  - Se a resposta não puder ser encontrada no contexto, apenas não responda.
+  - Seja objetivo;
+  - Mantenha um tom educado e profissional;
+  - Cite trechos relevantes do contexto somente quando necessário e apropriado;
+  - Corrija sintaxe, gramática e pontuação, se necessário;
+  - Se for citar o contexto, utilize o termo "conteúdo da aula";
+  `.trim();
+
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: prompt,
+      },
+    ],
+  });
+
+  if (!response.text) {
+    throw new Error('Failed to generate answer');
+  }
+
+  return response.text;
+}
